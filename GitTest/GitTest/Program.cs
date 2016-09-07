6d128a730b9c7e3efb35039d1dc163a2e5368c0a
geecol <gucollin@gmail.com>
2016-09-07 11:38:00 +0200

    /g

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -10,7 +10,7 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej");
+            Console.WriteLine("Hej /g");
             Console.ReadLine();
 
         }
